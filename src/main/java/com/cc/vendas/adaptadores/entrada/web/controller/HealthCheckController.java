95f2967899646e1b547863720b9db6a23bf4b65b
Jonathan Ferreira <jhouus33@gmail.com>
2026-02-28 12:35:55 -0300

    fiz: ajuste arquivos k8s

--- src/main/java/com/cc/vendas/adaptadores/entrada/web/controller/HealthCheckController.java
+++ src/main/java/com/cc/vendas/adaptadores/entrada/web/controller/HealthCheckController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
-public class TestController {
+@RequestMapping("/ping")
+public class HealthCheckController {
 
     @GetMapping
     public String test() {
